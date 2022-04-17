4f593e30083ebe151e400beb96871d0520b70aee
Felix Groll <felix.groll@gmail.com>
2022-04-17 10:18:56 +0200

    🩹 fixed #1: centered
    - minor cleanup

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 let x =0 ;
 
 function setup() {
-  createCanvas(500, 500);
+  let canvas = createCanvas(500, 500);
+  canvas.parent("canvascontainer");
+
 }
 
 function draw() {
   clear();
-  //let x = constrain(map(mouseX, 20, width, 0, 100), 0, 100);
   let a = map(x, 0, 100, 0, 250);
 
   select("#poti-top").style("transform", "rotate(" + a + "deg");
@@ -21,27 +22,6 @@ function draw() {
   stroke(75);
   let deg, p, cy;
   
-  /*
-  deg = map(x, 0, 100, 0, 125) + 55 + 90;
-  p = cartesian(deg - 10, 150);
-  line(50, 250 + p.y, 250 + p.x, 250 + p.y);
-  line(50, 380, 150, 380);
-  cy = min((415 - (250 + p.y)) / 2 + (p.y + 250), 368);
-  line(50, cy - 11, 50, 250 + p.y);
-  line(50, cy + 11, 50, 380);
-  select("#r1").position(20, cy - 8);
-  select("#r1").html("<span class='centertext'>" + (100*(x)).toK()+"&#8486;</span>");
-
-  deg = map(x, 0, 100, 125, 250) + 55 + 90;
-  p = cartesian(deg + 10, 150);
-  line(450, 250 + p.y, 250 + p.x, 250 + p.y);
-  line(450, 380, 350, 380);
-  cy = (380 - (250 + p.y)) / 2 + (p.y + 250);
-  line(450, cy - 11, 450, 250 + p.y);
-  line(450, cy + 11, 450, 380);
-  select("#r2").position(420, cy - 8);
-  select("#r2").html("<span class='centertext'>" + (100*(100-x)).toK()+"&#8486;</span>");
-*/
   noFill();
   stroke(255, 200, 0,127);
   strokeWeight(5);
@@ -54,22 +34,15 @@ function draw() {
   if (x > 1)
     arc(250, 250, 320, 320, radians(145), radians(145 + a - 2), OPEN);
 
-  //deg = map(x, 0, 100, 0, 125) + 55 + 90;
-  //p = cartesian(deg - 10, 150);
-  p = cartesian(min(max(10,a-60),240)+55+90, 220);
-  select("#r1").position(p.x+225, p.y+260);
+  let margin = (windowWidth-500)/2;
 
-  //cy = min((415 - (250 + p.y)) / 2 + (p.y + 250), 368);
-  //select("#r1").position(40, map(x,0,100,260,150));
+  p = cartesian(min(max(10,a-60),240)+55+90, 220);
+  select("#r1").position(margin+p.x+225, p.y+260);
   select("#r1").html("<span class='centertext'>" + (100*(x)).toK()+"&#8486;</span>");
 
   p = cartesian(min(max(0,a+60),240)+55+90, 220);
-
-console.log(deg);
-  //select("#r2").position(400, 230);
-  select("#r2").position(p.x+215, p.y+260);
+  select("#r2").position(margin+p.x+215, p.y+260);
   select("#r2").html("<span class='centertext'>" + (100*(100-x)).toK()+"&#8486;</span>");
-
 }
 
 function divideVoltage(rH, rL, vIn) {
